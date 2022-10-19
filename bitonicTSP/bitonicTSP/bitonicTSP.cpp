9d20247b4cddf0cd0fb942824070acc4d107f672
Aidan Justice <aidan.justice@mines.sdsmt.edu>
2022-10-19 13:14:33 -0600

    created brute force method

--- bitonicTSP/bitonicTSP/bitonicTSP.cpp
+++ bitonicTSP/bitonicTSP/bitonicTSP.cpp
@@ -1,7 +1,9 @@
+#include <cmath>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 struct City{
@@ -10,29 +12,76 @@ struct City{
     bool visited = false;
 };
 
-bool readFile( string fileName, vector<City>& cities, int& numCities );
+vector<int> bruteForceTSP(vector<City> cities, int numCities, double& dist);
 double dist(City city1, City city2);
-double bruteForceTSP(vector<City> cities, int numCities, int from, int to);
+bool isValid(vector<int> set);
+vector<vector<int>> genSubsets(int numCities);
+bool readFile( string fileName, vector<City>& cities, int& numCities );
+
 
 int main(int argc, char**argv)
 {
+    int i;
     int numCities;
+    double bestDist;
     vector<City> cities;
-    vector<double> distances;
+    vector<int> bestPath;
     string fileName = argv[1];
+    ofstream fout;
 
     if ( !readFile( fileName, cities, numCities ) )
         return -1;
+    
+    fileName.erase(fileName.end() - 3, fileName.end());
+    fout.open(fileName + ".out");
 
-    distances.resize(numCities);
+    bestPath = bruteForceTSP(cities, numCities, bestDist);
+
+    cout << "Final Path: {"; 
+    for (i = 0; i < numCities; i++)
+    {
+        cout << bestPath[i] << " ";
+    }    
+    cout << "}" << endl << "Distance: " << bestDist;
 
     return 0;
 }
 
-/*double bruteForceTSP(vector<City> cities, int numCities, int from, int to) {
-    if (cities[to].visited)
-        return -1;
-}*/
+vector<int> bruteForceTSP(vector<City> cities, int numCities, double& bestDist)
+{
+    int i, j;
+    double tempDist = 0;
+    vector<int> bestPath(numCities);
+    vector<int> currPath(numCities);
+    vector<vector<int>> subsets;
+
+    subsets = genSubsets(numCities);
+
+    bestPath = subsets[0];
+    bestDist = 0;
+    for (i = 1; i < numCities; i++)
+    {
+        bestDist += dist(cities[i], cities[i - 1]);
+    }   
+    bestDist += dist(cities[numCities - 1], cities[0]);
+
+    for (i = 1; i < subsets.size(); i++)
+    {
+        tempDist = 0;
+        for (j = 1; j < numCities; j++)
+        {
+            tempDist += dist(cities[subsets[i][j]], cities[subsets[i][j - 1]]);
+        }
+        tempDist += dist(cities[subsets[i][0]], cities[subsets[i][numCities - 1]]);
+        if (tempDist < bestDist)
+        {
+            bestDist = tempDist;
+            bestPath = subsets[i];
+        }
+    }
+
+    return bestPath;
+}
 
 double dist(City city1, City city2) {
     double x = city1.x - city2.x;
@@ -40,6 +89,51 @@ double dist(City city1, City city2) {
     x = pow(x,2);
     y = pow(y,2);
     return sqrt(x + y);
+}
+
+bool isValid(vector<int> set)
+{
+    int i = 2;
+    if (!(set[0] == 0 && set[1] == 1))
+        return false;
+
+    while( set[i] != set.size() - 1)
+    {
+        if (set[i] < set[i-1]) {
+            return false;
+        }
+        i++;
+    }
+    i++;
+    while( i < set.size())
+    {
+        if (set[i] > set[i - 1]) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
+vector<vector<int>> genSubsets(int numCities)
+{
+    int i;
+    vector<int> subset(numCities);
+    vector<vector<int>> allSubsets;
+    
+    for (i = 0; i < numCities; i++)
+    {
+        subset[i] = i;
+    }
+
+    do {
+        if (isValid(subset))
+        {
+            allSubsets.push_back(subset);
+        }
+    } while ( next_permutation(subset.begin(), subset.end()));
+
+    return allSubsets;
 }
 
 bool readFile( string fileName, vector<City>& cities, int& numCities ) {
@@ -59,4 +153,3 @@ bool readFile( string fileName, vector<City>& cities, int& numCities ) {
 
     return true;
 }
-
